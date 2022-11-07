b5e1c57e7cb74c50acb7c6b232e0e800538cc44b
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-11-07 13:25:41 +0100

    Binding model articles page

--- MyBlogInitiation/Controllers/ArticlesController.cs
+++ MyBlogInitiation/Controllers/ArticlesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBlogInitiation.Models;
 
 namespace MyBlogInitiation.Controllers
 {
@@ -6,7 +7,36 @@ namespace MyBlogInitiation.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            //Créer une liste d'article EN DUR
+            var vm = new ArticlesViewModel
+            {
+                Articles = new List<ArticleModel>
+                {
+                    new ArticleModel
+                    {
+                        Id =0,
+                        Title = "Les objets connectés en 2022",
+                        Content = "...",
+                        Available = true
+                    },
+                    new ArticleModel
+                    {
+                        Id =1,
+                        Title = "Les objets connectés en 2023",
+                        Content = "...",
+                        Available = true
+                    },
+                    new ArticleModel
+                    {
+                        Id =2,
+                        Title = "Les objets connectés en 2024",
+                        Content = "...",
+                        Available = false
+                    }
+                }
+            };
+
+            return View(vm);
         }
     }
 }
---
+++ MyBlogInitiation/Models/ArticlesViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MyBlogInitiation.Models
+{
+    //Une classe que je veux donner à la vue
+    public class ArticlesViewModel
+    {
+        public List<ArticleModel> Articles { get; set; }
+    }
+
+
+    //Une classe identique à ma table en BDD
+    public class ArticleModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool Available { get; set; }
+    }
+}
