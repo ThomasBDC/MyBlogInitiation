3243b3d2556eac97b052a58a6bc9d59edc4d62d7
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-11-22 13:15:31 +0100

    Récupération des articles en BDD via Entity

--- MyBlogInitiation/Controllers/ArticlesController.cs
+++ MyBlogInitiation/Controllers/ArticlesController.cs
@@ -1,20 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MyBlogInitiation.Mocks;
 using MyBlogInitiation.Models;
+using MyBlogInitiation.Repository.Context;
 
 namespace MyBlogInitiation.Controllers
 {
     public class ArticlesController : Controller
     {
-        public IActionResult Index()
+        private readonly DbBlogContext _dbBlogContext;
+        public ArticlesController(DbBlogContext dbBlogContext)
+        {
+            _dbBlogContext = dbBlogContext;
+        }
+
+        public async Task<IActionResult> Index()
         {
             //Créer une liste d'article EN DUR
             var vm = new ArticlesViewModel
             {
-                Articles = ArticlesMock.listArticles
+                Articles = await _dbBlogContext.Articles.ToListAsync()
             };
 
             return View(vm);
         }
+
+
     }
 }
