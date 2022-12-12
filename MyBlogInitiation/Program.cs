bcda83af47c62a6ec7c2c4ee355597e96a457b54
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-12-12 13:17:04 +0100

    Add mail service for authentication

--- MyBlogInitiation/Program.cs
+++ MyBlogInitiation/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 using MyBlogInitiation.Data;
 using MyBlogInitiation.Repository.DAL;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MyBlogInitiation.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DbBlogContext>(options =>
   options.UseSqlServer(builder.Configuration.GetConnectionString("BlogDbContext")));
 
+
+///Solution -> Changer la classe IdentityUser dans le code behind de la page cr��e
 builder.Services.AddDefaultIdentity<MyBlogInitiationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<DbBlogContext>();
@@ -33,6 +37,9 @@ if (builder.Environment.IsDevelopment())
 {
     mvcBuilder.AddRazorRuntimeCompilation();
 }
+
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+builder.Services.Configure<AuthMessageSenderOptions>(builder.Configuration);
 
 var app = builder.Build();
 
